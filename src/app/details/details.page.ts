9c660824999f9ba525964ba3e833ff039d2d5e9c
Laz <lazgerasimov@gmail.com>
2024-01-09 20:35:24 +0200

    update details component scaffold

--- src/app/details/details.page.ts
+++ src/app/details/details.page.ts
@@ -1,7 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit, WritableSignal, inject, signal } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
+import { MovieService } from '../services/movie.service';
+import { MovieResult } from '../interfaces';
 
 @Component({
   selector: 'app-details',
@@ -10,7 +12,19 @@ import { IonicModule } from '@ionic/angular';
   standalone: true,
   imports: [IonicModule, CommonModule, FormsModule]
 })
-export class DetailsPage implements OnInit {
+export class DetailsPage {
+
+  private movieSerivce = inject(MovieService);
+  public imageBaseUrl = 'https//image.tmdb.org/t/p';
+  public movie: WritableSignal<MovieResult | null> = signal(null);
+
+  @Input()
+  set id(movieId: string) {
+    this.movieSerivce.getMovieDetails(movieId).subscribe((movie) => {
+      console.log(movie)
+      this.movie.set(movie);
+    });
+  }
 
   constructor() { }
 
